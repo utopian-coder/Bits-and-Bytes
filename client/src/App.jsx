2cd2173444f3181a98edd65326e3a58e506eac92
Imran Biswas <imranbiswas2209@gmail.com>
2023-07-09 09:55:43 +0530

    Update client-side env variables, set default axios request rule and base url.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,9 @@
 import "./App.css";
+import axios from "axios";
+
+//Setting default rules for axios requests
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL; //http://127.0.0.1:4000/api/v1
+axios.defaults.withCredentials = true; //For including cookies in request header
 
 function App() {
   return <div>Bits and Bytes</div>;
