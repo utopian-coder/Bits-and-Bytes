795486fafaae23c5d4459b02b2a5e93b17f8324b
Imran Biswas <imranbiswas2209@gmail.com>
2023-07-15 19:54:59 +0530

    Stop footer from being rendered in every page.

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../components/Hero/Hero";
 import Features from "../components/Features/Features";
 import Courses from "../components/Courses/Courses";
+import Footer from "../components/Footer/Footer";
 
 const HomePage = () => {
   return (
